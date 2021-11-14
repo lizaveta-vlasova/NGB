3ddf5a57122f02e6b27ae15a0c1230e2034f8713
Roman Gorokhovsky <76645646+madmongoose@users.noreply.github.com>
2021-11-14 23:07:27 +0300

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -31,12 +31,12 @@ if [ $? -ne 0 ]; then
     exit 0
 fi
 
-echo $DOCKER_USER
-echo $DOCKER_PSWD
-echo $NGB_VERSION
+echo "username $DOCKER_USER"
+echo "password $DOCKER_PSWD"
+echo "version $NGB_VERSION"
 
 if [[ "$GITHUB_REF_NAME" == "release/"* ]]; then
-  docker login -u $DOCKER_USER --password-stdin $DOCKER_PSWD
+  docker login -u $DOCKER_USER -p $DOCKER_PSWD
   docker tag ngb:latest $DOCKER_USER/ngb:$NGB_VERSION
   docker push $DOCKER_USER/ngb:$NGB_VERSION
 fi
